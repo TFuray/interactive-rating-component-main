7347178cfecec34b548b085544c1c3920dca33ba
Thomas Furay <103867568+TFuray@users.noreply.github.com>
2023-08-31 12:09:01 -0700

    init upload

---
+++ interactive-rating-component/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import RatingBox from "./components/rating/RatingBox"
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center ">
+        <RatingBox />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ interactive-rating-component/src/components/rating/RatingBox.jsx
@@ -0,0 +1,10 @@
+const RatingBox = () => {
+  return (
+    <>
+      <div className="h-1/4 w-1/4 p-48 mt-48 rounded-3xl bg-slate-700">
+
+      </div>
+    </>
+  )
+}
+export default RatingBox
